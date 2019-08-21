8ba74ddc46212b56f0d45f0e40795cc4074487b1
infix1 <40860821+infix@users.noreply.github.com>
2019-08-22 01:51:25 +0200

    Added browser alarm to constantly check for updates. and added notifications.

--- src/background/index.ts
+++ src/background/index.ts
@@ -53,3 +53,63 @@ browser.runtime.onMessage.addListener(async (message: Message) => {
   }
 });
 
+const notificationsMap = new Map<string, string>();
+
+function getNewItems(oldArr: Parser.Item[], newArr: Parser.Item[]) {
+  const arrToDict = arr => arr.reduce((acc, cur) => {
+    acc[cur.id] = cur;
+    return acc;
+  }, {});
+
+  const oldDict = arrToDict(oldArr);
+  const newDict = arrToDict(newArr);
+
+  return Object.keys(newDict).filter(id => !oldDict[id])
+    .map(id => newDict[id]);
+}
+
+function notifyNewItems(items: Parser.Item[]) {
+  items.forEach(async ({ title, link }) => {
+    const iconUrl = `https://s2.googleusercontent.com/s2/favicons?domain=${link}`;
+
+    const notificationId = await browser.notifications.create({
+      title,
+      type: "basic",
+      message: "",
+      contextMessage: "uRSS",
+      iconUrl,
+    });
+
+    notificationsMap.set(notificationId, link);
+  });
+}
+
+browser.notifications.onClicked.addListener(async (id) => {
+  await browser.tabs.create({ url: notificationsMap.get(id) });
+  await browser.notifications.clear(id);
+  notificationsMap.delete(id);
+});
+
+browser.notifications.onClosed.addListener((id) => {
+  notificationsMap.delete(id);
+});
+
+const periodInMinutes = process.env.NODE_ENV == "production" ? 1 : 0.1;
+
+browser.alarms.create("CHECK_UPDATES", { periodInMinutes });
+
+browser.alarms.onAlarm.addListener(async (alarm) => {
+  if (alarm.name == "CHECK_UPDATES") {
+    const feedList: any = await localForage.getItem("feedList");
+    const updatedFeedList = feedList.map(async feed => {
+      const { items } = await parser.parseURL(feed.url);
+      const newItems = getNewItems(feed.items, items);
+      notifyNewItems(newItems);
+      feed.items = [...newItems, ...feed.items];
+      return feed;
+    });
+
+    await localForage.setItem("feedList", await Promise.all(updatedFeedList));
+  }
+});
+
--- src/popup/components/FeedListItem.tsx
+++ src/popup/components/FeedListItem.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 import { Text } from "rebass";
 
-type props = { id: string, url: string };
-export const FeedListItem: React.FC<props> = ({ id, url }) => (
+type props = { id: string, url: string, title: string; };
+export const FeedListItem: React.FC<props> = ({ id, title }) => (
   <RouterLink to={`/feed/${id}`}>
-    <Text m={1} color="primary">{url}</Text>
+    <Text m={1} color="primary">{title}</Text>
   </RouterLink>
 );
