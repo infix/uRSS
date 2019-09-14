401391be38be63710694591a0aa9403e32cfa4da
infix <40860821+infix@users.noreply.github.com>
2019-09-14 16:06:49 +0200

    Moved message handler to a seperate file

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,10 +1,9 @@
 import { browser } from "webextension-polyfill-ts";
-import { Message } from "../shared/Message";
 import localForage from "localforage";
-import nanoid from "nanoid";
 import Parser from "rss-parser";
+import { messageHandler } from "./messageHandler";
 
-const parser = new Parser();
+export const parser = new Parser();
 
 localForage.config({
   driver: localForage.LOCALSTORAGE,
@@ -19,63 +18,7 @@ browser.runtime.onInstalled.addListener(async details => {
   }
 });
 
-browser.runtime.onMessage.addListener(async (message: Message) => {
-  switch (message.type) {
-    case "GET_FEED_LIST": {
-      return await localForage.getItem("feedList");
-    }
-    case "ADD_FEED_ITEM": {
-      const { url } = message.payload;
-      const feedList: any = await localForage.getItem("feedList");
-      const { description, title, items, link } = await parser.parseURL(url);
-
-      const newItem = {
-        id: nanoid(), url, description, title, items, link,
-      };
-      await localForage.setItem("feedList", [...feedList, newItem]);
-      return newItem;
-    }
-    case "REMOVE_FEED_ITEM": {
-      const { id } = message.payload;
-      const feedList: any = await localForage.getItem("feedList");
-      const newFeedList = feedList.filter(item => item.id != id);
-      return await localForage.setItem("feedList", newFeedList);
-    }
-    case "GET_FEED_ITEM": {
-      const { id } = message.payload;
-      const feedList: any = await localForage.getItem("feedList");
-      return feedList.find(item => item.id == id);
-    }
-    case "UPDATE_FEED_ITEM": {
-      const { id, data } = message.payload;
-      const feedList: any[] = await localForage.getItem("feedList");
-      const index = feedList.findIndex(feed => feed.id == id);
-      console.log({ index, feed: feedList[index], data });
-      feedList[index] = { ...feedList[index], ...data };
-      return await localForage.setItem("feedList", feedList);
-    }
-    case "SORT_FEED_LIST": {
-      // create a dictionary that holds the mapping between id, and index
-      const order = message.payload.order
-        .reduce((ids, currId, index) => {
-          ids[currId] = index;
-          return ids;
-        }, {});
-
-      // Nothing to do.
-      if (Object.keys(order).length == 0)
-        return;
-
-      const feedList: any[] = await localForage.getItem("feedList");
-      const sortedFeedList = feedList
-        .sort((a, b) => order[a.id] - order[b.id]);
-
-      return await localForage.setItem("feedList", sortedFeedList);
-    }
-    default:
-      return;
-  }
-});
+browser.runtime.onMessage.addListener(messageHandler);
 
 const notificationsMap = new Map<string, string>();
 
---
+++ src/background/messageHandler.ts
@@ -0,0 +1,76 @@
+import localForage from "localforage";
+import nanoid from "nanoid";
+import { parser } from "./index";
+import { Message } from "../shared/Message";
+
+async function getFeedList() {
+  return await localForage.getItem("feedList");
+}
+
+async function addFeedItem(message) {
+  const { url } = message.payload;
+  const feedList: any = await localForage.getItem("feedList");
+  const { description, title, items, link } = await parser.parseURL(url);
+
+  const newItem = { id: nanoid(), url, description, title, items, link };
+  await localForage.setItem("feedList", [...feedList, newItem]);
+  return newItem;
+}
+
+async function removeFeedItem(message) {
+  const { id } = message.payload;
+  const feedList: any = await localForage.getItem("feedList");
+  const newFeedList = feedList.filter(item => item.id != id);
+  return await localForage.setItem("feedList", newFeedList);
+}
+
+async function getFeedItem(message) {
+  const { id } = message.payload;
+  const feedList: any = await localForage.getItem("feedList");
+  return feedList.find(item => item.id == id);
+}
+
+async function updateFeedItem(message) {
+  const { id, data } = message.payload;
+  const feedList: any[] = await localForage.getItem("feedList");
+  const index = feedList.findIndex(feed => feed.id == id);
+  console.log({ index, feed: feedList[index], data });
+  feedList[index] = { ...feedList[index], ...data };
+  return await localForage.setItem("feedList", feedList);
+}
+
+async function sortFeedList(message) {
+  // create a dictionary that holds the mapping between id, and index
+  const order = message.payload.order
+    .reduce((ids, currId, index) => {
+      ids[currId] = index;
+      return ids;
+    }, {});
+
+  // Nothing to do.
+  if (Object.keys(order).length == 0)
+    return;
+
+  const feedList: any[] = await localForage.getItem("feedList");
+  const sortedFeedList = feedList
+    .sort((a, b) => order[a.id] - order[b.id]);
+
+  return await localForage.setItem("feedList", sortedFeedList);
+}
+
+export const messageHandler = async (message: Message) => {
+  switch (message.type) {
+    case "GET_FEED_LIST":
+      return await getFeedList();
+    case "ADD_FEED_ITEM":
+      return await addFeedItem(message);
+    case "REMOVE_FEED_ITEM":
+      return await removeFeedItem(message);
+    case "GET_FEED_ITEM":
+      return await getFeedItem(message);
+    case "UPDATE_FEED_ITEM":
+      return await updateFeedItem(message);
+    case "SORT_FEED_LIST":
+      return await sortFeedList(message);
+  }
+};
